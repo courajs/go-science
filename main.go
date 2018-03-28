4ef75be05d3d345638a8cc8646cebca09ed585b8
Aaron Sikes <aaron@sikes.io>
2018-03-28 08:23:12 -0400

    Practice: K-nearest point algorithm
    This could be improved a touch using a heap. I could check out
    "container/heap"
    
    https://www.youtube.com/watch?v=eaYX0Ee0Kcg

--- main.go
+++ main.go
@@ -1,17 +1,75 @@
 package main
 
 import (
-	. "fmt"
+	"fmt"
+	"math"
+	"math/rand"
+	"sort"
 )
 
-func act() (int, int) {
-	return 1, 2
+type Point struct {
+	x, y int
 }
 
-func delegate() (int, int) {
-	return act()
+// Magnitude calculates distance from the origin
+func (p Point) Magnitude() float64 {
+	return math.Sqrt(float64(p.x*p.x + p.y*p.y))
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("{%v %v}:%.4g", p.x, p.y, p.Magnitude())
+}
+
+type Points []Point
+
+func (p Points) Last() Point {
+	return p[len(p)-1]
+}
+
+// Sort interface
+func (p Points) Len() int {
+	return len(p)
+}
+func (p Points) Less(i, j int) bool {
+	return p[i].Magnitude() < p[j].Magnitude()
+}
+func (p Points) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func getPoints() []Point {
+	points := make([]Point, 50)
+	for i := range points {
+		points[i] = Point{rand.Intn(50), rand.Intn(50)}
+	}
+	return points
+}
+
+func kNearest(points []Point, k int) []Point {
+	result := Points(make([]Point, k))
+
+	i := 0
+
+	for i < k {
+		result[i] = points[i]
+		i++
+	}
+
+	sort.Sort(result)
+
+	for i < len(points) {
+		if points[i].Magnitude() < result[k-1].Magnitude() {
+			result[k-1] = points[i]
+			sort.Sort(result)
+		}
+		i++
+	}
+
+	return result
 }
 
 func main() {
-	Println(delegate())
+	p := getPoints()
+	// fmt.Println(p)
+	fmt.Println(kNearest(p, 4))
 }
