2f7b8dfe8c42575aeea3fc53fbf8dd0899bac0f5
Aaron Sikes <aaron@sikes.io>
2018-04-20 16:15:37 -0400

    Verify a bcrypt password

--- main.go
+++ main.go
@@ -1,41 +1,24 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"os"
 
-	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type UserVerifier struct {
-	id                int
-	email             string
-	username          string
-	encryptedPassword string
-}
-
 func main() {
-	db, err := sql.Open("postgres", "postgres://focker:@localhost:5432/mlnptv_www_production?sslmode=disable")
-	if err != nil {
-		panic(err)
+	pw, ok := os.LookupEnv("PW")
+	if !ok {
+		panic("Pass PW")
 	}
-	rows, err := db.Query("SELECT id, email, username, encrypted_password FROM users LIMIT 10")
-	if err != nil {
-		panic(err)
+	hash, ok := os.LookupEnv("PW_HASH")
+	if !ok {
+		panic("Pass PW_HASH")
 	}
 
-	colTypes, err := rows.ColumnTypes()
-	if err != nil {
-		panic(err)
-	}
-	typeTypes := make([]string, len(colTypes))
-	for i, coltype := range colTypes {
-		typeTypes[i] = coltype.Name() + ":" + coltype.DatabaseTypeName()
-	}
-	fmt.Println(typeTypes)
-	var uv UserVerifier
-	for rows.Next() {
-		rows.Scan(&uv.id, &uv.email, &uv.username, &uv.encryptedPassword)
-		fmt.Println(uv)
-	}
+	nah := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw))
+
+	fmt.Println(nah, pw, hash)
+
 }
