9e7d7a238123eb0ed087b7ae55948edeaa4c2a31
Aaron Sikes <aaron@sikes.io>
2018-04-20 15:44:33 -0400

    Do some SQL

--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/url"
+
+	_ "github.com/lib/pq"
 )
 
+type UserVerifier struct {
+	id                int
+	email             string
+	username          string
+	encryptedPassword string
+}
+
 func main() {
-	url, err := url.ParseRequestURI("./hey")
+	db, err := sql.Open("postgres", "postgres://focker:@localhost:5432/mlnptv_www_production?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(url.Hostname())
+	rows, err := db.Query("SELECT id, email, username, encrypted_password FROM users LIMIT 10")
+	if err != nil {
+		panic(err)
+	}
+
+	colTypes, err := rows.ColumnTypes()
+	if err != nil {
+		panic(err)
+	}
+	typeTypes := make([]string, len(colTypes))
+	for i, coltype := range colTypes {
+		typeTypes[i] = coltype.Name() + ":" + coltype.DatabaseTypeName()
+	}
+	fmt.Println(typeTypes)
+	var uv UserVerifier
+	for rows.Next() {
+		rows.Scan(&uv.id, &uv.email, &uv.username, &uv.encryptedPassword)
+		fmt.Println(uv)
+	}
 }
